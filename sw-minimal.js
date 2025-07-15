600765cfb31f63acef6b720823e3a3f57320d867
Mariano Romero <mariano.es@gmail.com>
2025-07-15 16:22:22 +0200

    Icons

--- sw-minimal.js
+++ sw-minimal.js
@@ -1,17 +1,52 @@
 // Minimal Service Worker that works
-const CACHE_NAME = 'valpadel-v1';
+const CACHE_NAME = 'valpadel-v2';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/index.tsx',
+  '/ValPadelLogo.png',
+  '/favicon_io/favicon.ico',
+  '/favicon_io/favicon-16x16.png',
+  '/favicon_io/favicon-32x32.png',
+  '/favicon_io/android-chrome-192x192.png',
+  '/favicon_io/android-chrome-512x512.png',
+  '/favicon_io/apple-touch-icon.png',
+  '/manifest.json'
+];
 
 self.addEventListener('install', event => {
   console.log('SW: Install event');
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', event => {
   console.log('SW: Activate event');
-  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
-  // Just pass through to network - no caching for now
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
